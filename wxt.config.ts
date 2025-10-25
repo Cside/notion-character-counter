016b3584d36f3570722668530ae167fb068fd8da
Cside <cside.story@gmail.com>
2025-10-25 15:28:23 +0900

    options page

--- wxt.config.ts
+++ wxt.config.ts
@@ -1,8 +1,13 @@
-import { defineConfig } from 'wxt';
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig, WxtViteConfig } from "wxt";
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
   manifest: {
     permissions: ["webNavigation"],
-  }
+  },
+  vite: () =>
+    ({
+      plugins: [tailwindcss()],
+    } as WxtViteConfig),
 });
